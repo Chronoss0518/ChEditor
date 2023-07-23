549a6fffdeb5fc6afb8d8b259c2979feaefcc561
Chronoss <sinyusuke5000@gmail.com>
2023-07-23 14:20:59 +0900

    メニューボタンを作成(仮)

---
+++ resource.h
@@ -0,0 +1,26 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ で生成されたインクルード ファイル。
+// menu.rc で使用
+//
+#define FILE_NEW                        40001
+#define FILE_OPEN                       40002
+#define FILE_CLOSE                      40003
+#define APP_EXIT                        40004
+#define TEXT_SEARCH                     40005
+#define TEXT_COPY			            40006
+#define TEXT_CUT						40007
+#define TEXT_PASTE                      40008
+#define WINDOW_OPEN_PROPATY             40009
+#define WINDOW_OPEN_CLASSEDITOR         40010
+#define WINDOW_OPEN_FUNCTIONMANAGER     40011
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_COMMAND_VALUE         40013
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
