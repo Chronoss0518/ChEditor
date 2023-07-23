4f891c10d82c6dabbbab12205cc3bc75a9557e15
Chronoss <sinyusuke5000@gmail.com>
2023-07-23 14:20:37 +0900

    Control機能を統括するためのクラスを作成

---
+++ src/ControlManager.cpp
@@ -0,0 +1,13 @@
+﻿#include"BaseIncluder.h"
+#include"ControlManager.h"
+
+
+ChPtr::Shared<ChCpp::JsonObject> ControlManager::Serialize()
+{
+
+}
+
+void ControlManager::Deserialize(const ChPtr::Shared<ChCpp::JsonObject>& _json)
+{
+
+}
---
+++ src/ControlManager.h
@@ -0,0 +1,48 @@
+﻿#pragma once
+
+class ControlManager
+{
+public:
+
+	enum class ControlType
+	{
+		CHANGE_PROJECT_MATRIX_TYPE
+	};
+
+public:
+
+	ChPtr::Shared<ChCpp::JsonObject> Serialize();
+
+	void Deserialize(const ChPtr::Shared<ChCpp::JsonObject>& _json);
+
+	inline void SetCameraLookAngleRadian(float _radian)
+	{
+		if (_radian < 1.0f)return;
+		if (_radian >= 180.0f)return;
+		cameraLookAngleRadian = _radian;
+	}
+
+	inline float GetCameraLookAngleRadian()
+	{
+		return cameraLookAngleRadian;
+	}
+
+	inline void SetUseProjectionMatrixOrthographicFlg(bool _flg)
+	{
+		useProjectionMatrixOrthographicFlg = _flg;
+	}
+
+	inline bool IsUseProjectionMatrixOrthographicFlg() 
+	{
+		return useProjectionMatrixOrthographicFlg; 
+	}
+
+private:
+
+	float cameraLookAngleRadian = 60.0f;
+
+	bool useProjectionMatrixOrthographicFlg = false;
+
+	std::map<ControlType, std::vector<unsigned long>>shortcutKeyMap;
+
+};
