147fecf2cded9eb730ec25ab5522ffea2c5582a1
Chronoss <sinyusuke5000@gmail.com>
2023-07-23 14:26:40 +0900

    SerializeとDeserializeをちょっと修正

--- src/ControlManager.cpp
+++ src/ControlManager.cpp
@@ -4,10 +4,12 @@
 
 ChPtr::Shared<ChCpp::JsonObject> ControlManager::Serialize()
 {
+	auto&& res = ChPtr::Make_S<ChCpp::JsonObject>();
 
+	return res;
 }
 
 void ControlManager::Deserialize(const ChPtr::Shared<ChCpp::JsonObject>& _json)
 {
-
+	if (_json == nullptr)return;
 }
